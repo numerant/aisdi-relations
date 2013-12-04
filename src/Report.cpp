f7bef3d20fcd2eaf2cd012ffe711e5d12ecf1e97
Jakub Maleszewski <numerant@gmail.com>
2013-12-04 19:48:24 +0100

    [Kod] Dodano szczątkową klasę Report

---
+++ src/Report.cpp
@@ -0,0 +1,15 @@
+# Report class
+# This class contains report filled with various statistics (as the user demands).
+# Object of the class can be displayed by the interface as well as exported to the file.
+
+#include "Report.h"
+
+Report::Report()
+{
+    //ctor
+}
+
+Report::~Report()
+{
+    //dtor
+}
---
+++ src/Report.h
@@ -0,0 +1,18 @@
+# Report class
+# This class contains report filled with various statistics (as the user demands).
+# Object of the class can be displayed by the interface as well as exported to the file.
+
+#ifndef REPORT_H
+#define REPORT_H
+
+
+class Report
+{
+    public:
+        Report();
+        ~Report();
+    protected:
+    private:
+};
+
+#endif // REPORT_H
