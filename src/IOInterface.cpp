3ef13671d0d1c68fe8e8c0ec07b5fd468ee7449c
Jakub Maleszewski <numerant@gmail.com>
2013-12-04 19:39:52 +0100

    [Kod] Dodanie wstępnego projektu klasy IOInterface

---
+++ src/IOInterface.cpp
@@ -0,0 +1,14 @@
+# IOInterface class
+# This class is responsible for all file operations, like database import / export, e-mail import etc.
+
+#include "IOInterface.h"
+
+IOInterface::IOInterface()
+{
+    //ctor
+}
+
+IOInterface::~IOInterface()
+{
+    //dtor
+}
---
+++ src/IOInterface.h
@@ -0,0 +1,35 @@
+# IOInterface class
+# This class is responsible for all file operations, like database import / export, e-mail import etc.
+
+#ifndef IOINTERFACE_H
+#define IOINTERFACE_H
+
+#include <vector>
+
+class IOInterface
+{
+    public:
+
+    // Methods:
+        IOInterface();                                                              // trzeba zadbać o wskaźnik na bazę danych
+        ~IOInterface();
+
+        ImportStats importMail(string path, MailImportParam *parameters);
+
+        void exportDatabase (string file_path, ExportParam *parameters);            // można zrobić klasę Parameters, a ImportParamteres i ExportParameters bedą po niej dziedziczyć
+        void importDatabase (string file_path, DbImportParam * parameters);
+        void exportReport (string path, Report report);
+
+    private:
+    // Fields:
+        Database *database;                         // baza tworzona w momencie uruchomienia programu, czy przy imporcie maili?
+        Email *temp_email;
+        vector<char> *binary_file;                  // trzyma zserializowaną bazę danych w formie binarnej
+
+    // Methods:
+        void emlParser (string eml_content);        //sprawdza poprawność maili
+        void encryptFile (string password);
+        void decryptFile (string password);
+};
+
+#endif // IOINTERFACE_H
