e23d9ebc9011f16aa616d4f6e231a9dc9bdc4c14
Jakub Maleszewski <numerant@gmail.com>
2013-12-06 23:16:19 +0100

    [IOInterface] Dodany algorytm importu maili + niewielkie zmiany w koncepcji

--- src/IOInterface.cpp
+++ src/IOInterface.cpp
@@ -1,14 +1,73 @@
 // IOInterface class
 // This class is responsible for all file operations, like database import / export, e-mail import etc.
 
+// Klasa IOInterface
+// Odpowiedzialna za operacje na plikach, takie jak wczytywanie maili do bazy danych, import / eksport bazy, zapisywanie raportów do pliku
+
 #include "IOInterface.h"
 
-IOInterface::IOInterface()
+using namespace std;
+
+IOInterface::ImportStats::ImportStats()
 {
-    //ctor
+    successCount = 0;
+    failCount = 0;
+    existingCount = 0;
 }
 
-IOInterface::~IOInterface()
+ImportStats IOInterface::importMail(MailParam *parameters)
 {
-    //dtor
+    Email *tempEmail;       //pamiętać o delete!
+    ImportStats stats;
+
+
+    if (parameters->isDirectory)
+    {
+        boost::filesystem::path currentDir(parameters->path);                           // stworzenie boostowej zmiennej określającej ścieżkę
+
+        if (parameters->recursiveImport)
+            boost::filesystem::recursive_directory_iterator iter(currentDir), end;      // boostowy iterator przechodzący katalog i podkatalogi
+        else
+            boost::filesystem::directory_iterator iter(currentDir), end;                // ten pomija podkatalogi
+
+        for (; iter != end; ++iter)                                                     // przejechanie iteratorem po katalogu
+            if (iter->path().extension() == ".eml")
+            {
+                try tempEmail = emlParser( iter->path() );
+                catch ( /* mail nie jest poprawny */ )
+                {
+                    stats->failCount++;
+                    break;
+                }
+
+                try database->addEmail(*tempEmail)
+                catch ( /* mail istnieje już w bazie */ )
+                {
+                    stats->existingCount++;
+                    break;
+                }
+            }
+            delete tempEmail;   //break przerwie chyba tylko blok if, więc może być tutaj?
+    }
+    else
+    {
+        try tempEmail = emlParser(parameters->path);
+        catch ( /* mail nie jest poprawny */ )
+        {
+            stats->failCount++;
+            break;                      //wyskoczenie z bloku else
+        }
+
+        try database->addEmail(*tempEmail)
+        catch ( /* mail istnieje już w bazie */ )
+        {
+            stats->existingCount++;
+            delete tempEmail;
+            break;
+        }
+        stats->successCount++;
+        delete tempEmail;
+    }
+
+    return stats;
 }
--- src/IOInterface.h
+++ src/IOInterface.h
@@ -1,35 +1,50 @@
 // IOInterface class
 // This class is responsible for all file operations, like database import / export, e-mail import etc.
 
+// Klasa IOInterface
+// Odpowiedzialna za operacje na plikach, takie jak wczytywanie maili do bazy danych, import / eksport bazy, zapisywanie raportów do pliku
+
 #ifndef IOINTERFACE_H
 #define IOINTERFACE_H
 
 #include <vector>
+#include <fstream>
+#include "boost/filesystem.hpp"         // do wczytywania plików z katalogu - także rekursywnie
+#include "Database.h"
+#include "Email.h"
+#include "Param.h"
+
+using namespace std;
 
 class IOInterface
 {
     public:
-    // Classes
-        struct ImportStats { }                                                      // IOInterface::ImportStats
+    // Classes:
+        struct ImportStats                                                          // struktura zawierająca statystyki importu - ile udało się wczytać, ile nie itp.
+        {
+            ImportStats();
+            unsigned int successCount;                                              // niech ktoś mądry się wypowie czy tak jest OK, czy lepiej bawić się w set/get
+            unsigned int failCount;
+            unsigned int existingCount;                                             // maile, które już istniały w bazie
+        };
 
     // Methods:
         IOInterface();                                                              // trzeba zadbać o wskaźnik na bazę danych
         ~IOInterface();
 
-        ImportStats importMail(string path, MailImportParam *parameters);
+        ImportStats importMail(MailParam *parameters);
 
-        void exportDatabase (string filePath, ExportParam *parameters);            // można zrobić klasę Param, a ImportParam i ExportParam bedą po niej dziedziczyć
-        void importDatabase (string filePath, DbImportParam * parameters);
+        void exportDatabase (string filePath, DbParam *parameters);                 // opis klas MailParam i DbParam jest w Param.h
+        void importDatabase (string filePath, DbParam *parameters);
         void exportReport (string path, Report report);
 
     private:
     // Fields:
         Database *database;                         // baza tworzona w momencie uruchomienia programu, czy przy imporcie maili?
-        Email *tempEmail;
-        vector<char> *binary_file;                  // trzyma zserializowaną bazę danych w formie binarnej
+        //vector<char> *binary_file;                // trzyma zserializowaną bazę danych w formie binarnej - chyba lepiej tworzyć lokalnie, wewnątrz metody
 
     // Methods:
-        void emlParser (string emlContent);        //sprawdza poprawność maili
+        Email* emlParser (string path);             //sprawdza poprawność maili
         void encryptFile (string password);
         void decryptFile (string password);
 };
