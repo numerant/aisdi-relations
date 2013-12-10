f313982f359ec027ceda8311b72b6bc6060111f7
Maciek Safarzyński <msafarzynski@wp.pl>
2013-12-11 00:51:17 +0100

    Wstępna deklaracja klasy IOException

--- src/Exception/IOException.h
+++ src/Exception/IOException.h
@@ -3,11 +3,19 @@
 
 #ifndef IOEXCEPTION_H
 #define IOEXCEPTION_H
+#include <stdio.h>
+#include <string.h>
 
+class IOException : public exception
+{
+    protected:
+        string message;
+    public:
+        IOException(string);
+        string show();
+};
 
-class IOException : public exception {};
-
-class UnableToOpenFile : public IOException {};                 // można zaszaleć i jeszcze bardziej podzielić te wyjątki - np. zrobić jeszcze ParserException
+class UnableToOpenFile : public IOException{};                 // można zaszaleć i jeszcze bardziej podzielić te wyjątki - np. zrobić jeszcze ParserException
 class EmlSyntaxIncorrect : public IOException {};
 
 #endif // IOEXCEPTION_H
