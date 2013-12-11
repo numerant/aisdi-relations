ae4626517b4e1ae19a8b52f56c841fb59f9ba80b
PatrykLucka <patryk.lucka@gmail.com>
2013-12-11 20:43:44 +0100

    Dodanie konstruktora przetwarzającego string oraz metody przetwarzającej miesiąc

--- src/Date.h
+++ src/Date.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <string>
+#include <sstream>
 
 using namespace std;
 /*
-	Klasa przechowująca informacje dotyczące daty oraz operacje na niej.
+        Klasa przechowująca informacje dotyczące daty oraz operacje na niej.
 */
 
 class Date
@@ -13,7 +14,8 @@ class Date
     //dzień
     int day;
     //miesiąc
-    int month;
+    string month;
+	int monthInt;
     //rok
     int year;
     //godzina
@@ -23,7 +25,7 @@ class Date
     //sekunda
     int second;
     //strefa czasowa;
-    int timeZone;
+    string timeZone;
 
 
 public:
@@ -37,13 +39,70 @@ public:
         hour = date.hour;
         minute = date.minute;
     }
+
     Date (string newDate)
     {
+		string tempString;
+		stringstream stream;
+		//weekDay
+		stream>>newDate;
+		stream<<tempString;
+		tempString.erase(tempString.end()-1, tempString.end());
+		weekDay = tempString;
 
+		//day
+		stream>>newDate;
+		stream<<tempString;
+		day = atoi(tempString.c_str());
+
+		//month
+		stream>>newDate;
+		stream<<tempString;
+		month = tempString;
+		monthInt = monthToInt(month);
+
+		//year
+		stream>>newDate;
+		stream<<tempString;
+		year = atoi(tempString.c_str());
+
+		//hour:minute:second
+		stream>>newDate;
+		stream<<tempString;
+		string tempTime(tempString, 0, 2);
+		hour = atoi(tempTime.c_str());
+		string tempTime(tempString, 3, 2);
+		minute = atoi(tempTime.c_str());
+		string tempTime(tempString, 5, 2);
+		second = atoi(tempTime.c_str());
+
+		//timeZone
+		stream>>newDate;
+		stream<<timeZone;
     }
+
     ~Date() {};
 
-    string getweekDat()
+	string getWeekDate();
+	void setWeekDay(string );
+	int getDay();
+	void setDay(int newDay);
+	string getMonth();
+	void setMonth(string );
+	int getYear();
+	void setYear(int );
+	int getHour();
+	void setHour(int );
+	int getMinute();
+	void setMinute(int );
+	int getSecond();
+	void setSecond(int );
+	string getTimeZone();
+	void setTimeZone(string );
+	int compare(Date &);
+	int monthToInt(string );
+
+    string getWeekDate()
     {
         return weekDay;
     }
@@ -63,12 +122,12 @@ public:
         day = newDay;
     }
 
-    int getMonth()
+    string getMonth()
     {
         return month;
     }
 
-    void setMonth(int newMonth)
+    void setMonth(string newMonth)
     {
         month = newMonth;
     }
@@ -113,12 +172,12 @@ public:
         second = newSecond;
     }
 
-    int getTimeZone()
+    string getTimeZone()
     {
         return timeZone;
     }
 
-    void setTimeZone(int newTimeZone)
+    void setTimeZone(string newTimeZone)
     {
         timeZone = newTimeZone;
     }
@@ -131,9 +190,9 @@ public:
             return 1;
         if(year < date2.year)
             return -1;
-        if(month > date2.month)
+        if(monthInt > date2.monthInt)
             return 1;
-        if(month < date2.month)
+        if(monthInt < date2.monthInt)
             return -1;
         if(day > date2.day)
             return 1;
@@ -146,4 +205,31 @@ public:
         return 0;
     }
 
+	int monthToInt(string stringMonth)
+	{
+		if(stringMonth == "Jan")
+			monthInt = 1;
+		if(stringMonth == "Feb")
+			monthInt = 2;
+		if(stringMonth == "March")
+			monthInt = 3;
+		if(stringMonth == "Apr")
+			monthInt = 4;
+		if(stringMonth == "May")
+			monthInt = 5;
+		if(stringMonth == "June")
+			monthInt = 6;
+		if(stringMonth == "July")
+			monthInt = 7;
+		if(stringMonth == "Aug")
+			monthInt = 8;
+		if(stringMonth == "Sep")
+			monthInt = 9;
+		if(stringMonth == "Oct")
+			monthInt = 10;
+		if(stringMonth == "Nov")
+			monthInt = 11;
+		if(stringMonth == "Dec")
+			monthInt = 12;
+	}
 };
