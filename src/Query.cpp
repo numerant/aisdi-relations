a57a3634f896c45084100b62bb83a07e6993926a
Macielos <maciek.ganko@gmail.com>
2013-12-30 14:51:55 -0800

    [Klasy]Dodanie Query.cpp

--- src/Query.cpp
+++ src/Query.cpp
@@ -14,3 +14,93 @@ void Query::clear()
 {
 
 }
+
+IntCriteria* Query::getIntCriteria(int position)
+{
+    if(position>=0 && position<intCriteriaVector.size())
+        return &intCriteriaVector[position];
+    return nullptr;
+}
+
+StringCriteria* Query::getStringCriteria(int position)
+{
+    if(position>=0 && position<stringCriteriaVector.size())
+        return &stringCriteriaVector[position];
+    return nullptr;
+}
+
+DateCriteria* Query::getDateCriteria(int position)
+{
+     if(position>=0 && position<dateCriteriaVector.size())
+        return &dateCriteriaVector[position];
+    return nullptr;
+}
+
+int Query::getIntCriteriaVectorSize()
+{
+    return intCriteriaVector.size();
+}
+
+int Query::getStringCriteriaVectorSize()
+{
+    return stringCriteriaVector.size();
+}
+
+int Query::getDateCriteriaVectorSize()
+{
+    return dateCriteriaVector.size();
+}
+
+EmailQuery::EmailQuery()
+{
+
+}
+
+EmailQuery::~EmailQuery()
+{
+
+}
+
+GroupQuery::GroupQuery()
+{
+
+}
+
+GroupQuery::~GroupQuery()
+{
+
+}
+
+UsemberQuery::UsemberQuery()
+{
+
+}
+
+UsemberQuery::~UsemberQuery()
+{
+
+}
+
+void EmailQuery::clear()
+{
+    sortingOrder.clear();
+    intCriteriaVector.clear();
+    stringCriteriaVector.clear();
+    dateCriteriaVector.clear();
+}
+
+void GroupQuery::clear()
+{
+    sortingOrder.clear();
+    intCriteriaVector.clear();
+    stringCriteriaVector.clear();
+    dateCriteriaVector.clear();
+}
+
+void UsemberQuery::clear()
+{
+    sortingOrder.clear();
+    intCriteriaVector.clear();
+    stringCriteriaVector.clear();
+    dateCriteriaVector.clear();
+}
