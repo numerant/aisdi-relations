63c81631abb34fd06d449aacd1d5f2032149f21c
Macielos <maciek.ganko@gmail.com>
2013-12-30 14:28:11 -0800

    [Klasy]Zapytania + maly cleanup
    - Dodana obsluga zapytan (kompiluje sie, ale jeszcze nie testowana, testy beda jak Panda zrobi GUI)
    - Hermetyzacja
    - Komentarze
    - Ogolny porzadek

--- src/Database.h
+++ src/Database.h
@@ -1,10 +1,9 @@
 #pragma once
 #include "Email.h"
-#include "EmailQuery.h"
-#include "GroupQuery.h"
-#include "UsemberQuery.h"
+#include "Query.h"
 #include <vector>
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
 /*Klasa Database, przechowująca dane dotyczące grup, maili oraz użytkowników oraz udostępniająca
@@ -18,39 +17,45 @@ elementu.*/
 class Database
 {
 private:
-    vector<Email*> emailVector;             //wektor przechowujący obiekty klasy Email,
-    vector<Group*> groupVector;     //wektor przechowujący obiekty klasy Group
-    vector<Usember*> usemberVector;         //wektor przechowujący wszystkich nadawców i odbiorców emaili, czyli Usemberów
-
+    vector<Email*> emailVector;                 //wektor przechowujący obiekty klasy Email,
+    vector<Group*> groupVector;                 //wektor przechowujący obiekty klasy Group
+    vector<Usember*> usemberVector;             //wektor przechowujący wszystkich nadawców i odbiorców emaili, czyli Usemberów
+    vector<Email*> emailSearchResultVector;     //
+    vector<Group*> groupSearchResultVector;     //wektory wynikow wyszukiwania w bazie danych
+    vector<Usember*> usemberSearchResultVector; //
 public:
     Database();
     ~Database();
     bool addEmail(Email* email);                                //metoda dodająca element klasy Email do wektora emaili
-    bool addGroup(Group* group);                        //metoda dodająca element klasy Group do wektora grup
+    bool addGroup(Group* group);                                //metoda dodająca element klasy Group do wektora grup
     bool addUsember(Usember* usember);                          //metoda dodająca usembera do wektora usemberów
     void deleteEmail(Email* email);                             //metoda usuwająca podany e-mail z wektora
-    void deleteGroup(Group* group);                     //metoda usuwająca podany element klasy Group z wektora
+    void deleteGroup(Group* group);                             //metoda usuwająca podany element klasy Group z wektora
     void deleteUsember(Usember* usember);                       //metoda usuwająca podanego usembera z wektora
     vector<Email*>& select(EmailQuery& emailQuery);             //metody, które przeszukują bazę danych na podstawie danego obiektu klasy Query
-    vector<Group*>& select(GroupQuery& groupQuery); //przekazanego jako parametr, pobierają informacje z Query, tworzą odpowiednio posortowany wektor
+    vector<Group*>& select(GroupQuery& groupQuery);             //przekazanego jako parametr, pobierają informacje z Query, tworzą odpowiednio posortowany wektor
     vector<Usember*>& select(UsemberQuery& usemberQuery);       //wskaźników na maile/grupy/usemberów pasujących do kryteriów zapytania i zwraca referencję na niego                                                        //i zwracają referencję do niego
     void deleteQueryResults(Query* query);                      //metoda usuwająca wektor wyników zapytania utworzonych przez metodę select, wywoływana zaraz po niej
-    void sortEmails();                                          //metoda sortująca maile wg ID
-    void sortGroups();                                      //metoda sortująca grupy robocze
-    void sortUsembers();                                        //metoda sortująca usemberów alfabetycznie
-    int countEmails();                                          //metoda zwracająca liczbę maili w bazie
-    int countGroups();                                      //metoda zwracająca liczbę grup roboczych w bazie
-    int countUsembers();                                        //metoda zwracająca liczbę usemberów w bazie
-    int findEmail(string messageId);                                //metoda wyszukujące email w bazie
-    int findGroup(int groupId);                             //metoda wyszukujące grupę w bazie
+    int findEmail(string messageId);                            //metoda wyszukujące email w bazie
+    int findGroup(string groupId);                              //metoda wyszukujące grupę w bazie
     int findUsember(string name);                               //metoda wyszukujące usembera w bazie
     void clearEmails();                                         //metoda usuwająca wszystkie maile z bazy
-    void clearGroups();                                     //metoda usuwająca wszystkie grupy z bazy
+    void clearGroups();                                         //metoda usuwająca wszystkie grupy z bazy
     void clearUsembers();                                       //metoda usuwająca wszystkich usemberów z bazy
+    Email* getEmail(int position);                              //metoda zwracajaca mail na okreslonej pozycji w wektorze
+    Group* getGroup(int position);                              //metoda zwracajaca grupe na okreslonej pozycji w wektorze
+    Usember* getUsember(int position);                          //metoda zwracajaca usembera na okreslonej pozycji w wektorze
+    int countEmails();                                          //metoda zwracajaca ilosc maili w wektorze
+    int countGroups();                                          //metoda zwracajaca ilosc grup w wektorze
+    int countUsembers();                                        //metoda zwracajaca ilosc usemberow w wektorze
+private:
+    bool matches(Email& email, IntCriteria& intCriteria);       //
+    bool matches(Email& email, StringCriteria& stringCriteria); //metody sprawdzajace, czy podany mail spelnia podane kryterium
+    bool matches(Email& email, DateCriteria& dateCriteria);     //
     void swapEmails(int position1, int position2);              //metoda zamieniająca dwa maile w wektorze
-    void swapGroups(int position1, int position2);          //metoda zamieniająca dwie grupy w wektorze
+    void swapGroups(int position1, int position2);              //metoda zamieniająca dwie grupy w wektorze
     void swapUsembers(int position1, int position2);            //metoda zamieniająca dwóch usemberów w wektorze
-    Email* getEmail(int position);
-    Group* getGroup(int position);
-    Usember* getUsember(int position);
+    static bool compareEmails(Email* email1, Email* email2);    //metoda porownujaca 2 maile wg id, uzywana do sortowania
+    static bool compareGroups(Group* group1, Group* group2);    //metoda porownujaca 2 grupy wg id, uzywana do sortowania
+    static bool compareUsembers(Usember* usember1, Usember* usember2);  //metoda porownujaca 2 usemberow wg nazwy/imienia, uzywana do sortowania
 };
