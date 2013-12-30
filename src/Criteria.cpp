23e9e02d0a8b1ef5f94f6e47e632a9419a175d0a
Macielos <maciek.ganko@gmail.com>
2013-12-30 14:30:56 -0800

    [Klasy]Dodanie Criteria.cpp

--- src/Criteria.cpp
+++ src/Criteria.cpp
@@ -1,1 +1,77 @@
 #include "Criteria.h"
+
+SearchKey Criteria::getSearchKey()
+{
+    return searchKey;
+}
+
+IntCriteria::IntCriteria()
+{
+
+}
+
+IntCriteria::~IntCriteria()
+{
+
+}
+
+StringCriteria::StringCriteria()
+{
+
+}
+
+StringCriteria::~StringCriteria()
+{
+
+}
+
+DateCriteria::DateCriteria()
+{
+
+}
+
+DateCriteria::~DateCriteria()
+{
+
+}
+
+int IntCriteria::getEquals()
+{
+    return equals;
+}
+
+int IntCriteria::getLess()
+{
+    return less;
+}
+
+int IntCriteria::getGreater()
+{
+    return greater;
+}
+
+string StringCriteria::getName()
+{
+    return name;
+}
+
+Date* DateCriteria::getEquals()
+{
+    if(equals.getDay()==0 && equals.getMonthInt()==0 && equals.getYear()==0)
+        return nullptr;
+    return &equals;
+}
+
+Date* DateCriteria::getLess()
+{
+    if(less.getDay()==0 && less.getMonthInt()==0 && less.getYear()==0)
+        return nullptr;
+    return &less;
+}
+
+Date* DateCriteria::getGreater()
+{
+    if(greater.getDay()==0 && greater.getMonthInt()==0 && greater.getYear()==0)
+        return nullptr;
+    return &greater;
+}
