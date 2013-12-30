09100e7341368309b16a6d46b18b7f1be097de02
Macielos <maciek.ganko@gmail.com>
2013-12-30 14:51:51 -0800

    [Klasy]Dodanie klasy Query oraz pochodnych
    Wrzucilem pochodne Query do jednego pliku, kiedy byly oddzielnie to troche za duzo trzeba bylo przelaczac miedzy plikami przegladajac kod. Jak gdzies macie zaincludowane EmailQuery.h, GroupQuery.h lub UsemberQuery.h, to mozecie wywalic.

--- src/Query.h
+++ src/Query.h
@@ -6,16 +6,56 @@ using namespace std;
 
 /*  Klasa Query - czyli zapytanie wysyłane do bazy danych. Zawiera informacje na temat tego, po jakich kryteriach
     chcemy wyszukiwać elementy (emaile, grupy bądź usemberów) oraz jak (według kolejno jakich pól) mają być
-    posortowane wyniki. Kryteria mogą zawierać dokładną wartość danego pola, przedział, a także znaki *
+    posortowane wyniki. Kryteria mogą zawierać dokładną wartość danego pola badz w przypadku intow i dat przedział, a w przypadku stringow znaki *
     (dowolne znaki w nazwie). Na jej podstawie baza tworzy wektor wyników wyszukiwania.							 */
 
 class Query
 {
-private:
-    //vector<DataType> sortingOrder;
-    vector<Criteria> criteriaVector;
+protected:
+    vector<SearchKey> sortingOrder;                 //wektor przechowujacy klucz do posortowania wynikow wyszukiwania - sortowanie odbywa sie wg klucza na
+                                                    //1. pozycji, w przypadku elementow o takiej samej wartosci danego pola - wg klucza na 2. pozycji itd.
+    vector<IntCriteria> intCriteriaVector;          //
+    vector<StringCriteria> stringCriteriaVector;    //wektory kryteriow wyszukiwania
+    vector<DateCriteria> dateCriteriaVector;        //
+
 public:
     Query();
     ~Query();
+    virtual void clear();                           //metoda czyszczaca zapytanie
+    IntCriteria* getIntCriteria(int position);      //
+    StringCriteria* getStringCriteria(int position);//metody zwracajace pojedynczy element danego wektora
+    DateCriteria* getDateCriteria(int position);    //
+    int getIntCriteriaVectorSize();                 //
+    int getStringCriteriaVectorSize();              //metody zwracajace rozmiar danego wektora
+    int getDateCriteriaVectorSize();                //
+};
+
+class EmailQuery :
+    public Query
+{
+public:
+    EmailQuery();
+    ~EmailQuery();
     void clear();
 };
+
+class GroupQuery :
+    public Query
+{
+public:
+    GroupQuery();
+    ~GroupQuery();
+    void clear();
+};
+
+class UsemberQuery :
+    public Query
+{
+public:
+    UsemberQuery();
+    ~UsemberQuery();
+    void clear();
+};
+
+
+
